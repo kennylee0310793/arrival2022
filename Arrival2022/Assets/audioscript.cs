c6c645cc3fe2f1a35a7f138b3ac3ad083356049d
kennylee0310793 <32116605+kennylee0310793@users.noreply.github.com>
2022-04-06 16:49:59 +0800

    ver1.1

---
+++ Arrival2022/Assets/audioscript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class audioscript : MonoBehaviour
+{
+    AudioSource myAudio ;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myAudio = GetComponent<AudioSource>();
+        myAudio.PlayDelayed(10);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
